6014c9995bbe8372f3c2884ae5b46647481ddcde
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-04-18 10:13:01 -0300

    initial locales added

---
+++ src/config/i18n.js
@@ -0,0 +1,35 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import XHR from 'i18next-xhr-backend';
+
+import ptTranslation from '../locales/pt/translations.json';
+import enTranslation from '../locales/en/translations.json';
+import esTranslation from '../locales/es/translations.json';
+
+i18n
+  .use(XHR)
+  .use(LanguageDetector)
+  .init({
+    debug: true,
+    lng: 'pt',
+    fallbackLng: 'pt',
+    keySeparator: false,
+    interpolation: {
+      escapeValue: false,
+    },
+    resources: {
+      pt: {
+        translations: ptTranslation,
+      },
+      en: {
+        translations: enTranslation,
+      },
+      es: {
+        translations: esTranslation,
+      },
+    },
+    ns: ['translations'],
+    defaultNS: 'translations',
+  });
+
+export default i18n;
