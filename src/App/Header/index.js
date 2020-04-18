56dde57a09d27f37525f7a952f45b92905e63662
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-04-18 14:14:39 -0300

    Inital Header added

--- src/App/Header/index.js
+++ src/App/Header/index.js
@@ -3,6 +3,7 @@ import { Navbar, NavItem, Icon } from 'react-materialize';
 import { useHistory, Link } from 'react-router-dom';
 import i18n from 'i18next';
 
+
 function Header() {
   const history = useHistory();
 
@@ -15,7 +16,11 @@ function Header() {
   return (
     <Navbar
       alignLinks="right"
-      brand={<Link className="brand-logo" href="/">Logo</Link>}
+      brand={(
+        <Link className="brand-logo" href="/">
+          Logo
+        </Link>
+      )}
       id="mobile-nav"
       className="grey darken-1"
       menuIcon={<Icon>menu</Icon>}
@@ -27,12 +32,13 @@ function Header() {
         preventScrolling: true,
       }}
     >
-      <NavItem>Início</NavItem>
-      <NavItem>Sobre</NavItem>
+      <NavItem>{i18n.t('seeMap')}</NavItem>
+      <NavItem>{i18n.t('seeOrders')}</NavItem>
+      <NavItem>{i18n.t('aboutUs')}</NavItem>
       <NavItem divider />
       <NavItem style={{ marginLeft: '1rem' }}>
         <Icon tiny className="left">language</Icon>
-        Mudar o Idioma
+        {i18n.t('changeLang')}
       </NavItem>
     </Navbar>
   );
